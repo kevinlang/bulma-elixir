a5da17a8c9a761d5cf530a9282622307e5087462
Kevin Lang <me@gnalck.com>
2021-08-27 19:14:15 -0400

    init commit

---
+++ lib/bulma.ex
@@ -0,0 +1,18 @@
+defmodule Bulma do
+  @moduledoc """
+  Documentation for `Bulma`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bulma.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bulma_test.exs
@@ -0,0 +1,8 @@
+defmodule BulmaTest do
+  use ExUnit.Case
+  doctest Bulma
+
+  test "greets the world" do
+    assert Bulma.hello() == :world
+  end
+end
